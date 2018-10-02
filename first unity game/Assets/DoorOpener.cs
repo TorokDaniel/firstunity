ea9a9e75e2ee6f9740c3b8cb53fd8bd58866decf
Daniel Torok <torok.daniel@rarity.hu>
2018-10-02 18:04:26 +0200

    code formatting (whitespace)

--- first unity game/Assets/DoorOpener.cs
+++ first unity game/Assets/DoorOpener.cs
@@ -1,29 +1,27 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class DoorOpener : MonoBehaviour {
 
     public float openSpeed = 10;
-    public PressurePlate switchButton;
+    public PressurePlate pressurePlate;
     bool activated = false;
 
-	// Use this for initialization
-	void Start () {
-        switchButton.triggerDelegate = delegate () { activated = true; };
+    // Use this for initialization
+    void Start () {
+        pressurePlate.triggerDelegate = delegate () { activated = true; };
     }
-	
-	// Update is called once per frame
-	void Update () {
+
+    // Update is called once per frame
+    void Update () {
         if (!activated)
         {
             return;
         }
 
-		if (transform.eulerAngles.y < 90)
+        if (transform.eulerAngles.y < 90)
         {
             transform.Rotate(Vector3.up * Time.deltaTime * openSpeed);
         }
-	}
+    }
 
 }
--- first unity game/Assets/PressurePlate.cs
+++ first unity game/Assets/PressurePlate.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class PressurePlate : MonoBehaviour {
 
@@ -9,16 +7,17 @@ public class PressurePlate : MonoBehaviour {
 
     // Use this for initialization
     void Start () {
-		
+
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+    // Update is called once per frame
+    void Update () {
+
+    }
 
     private void OnTriggerEnter(Collider other)
     {
         triggerDelegate();
     }
+
 }
