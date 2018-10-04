154de72d188bd6ea49b1ef1d02844d111f485131
Daniel Torok <torok.daniel@rarity.hu>
2018-10-04 22:14:24 +0200

    multi-switch door

--- first unity game/Assets/DoorOpener.cs
+++ first unity game/Assets/DoorOpener.cs
@@ -1,16 +1,26 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 
 public class DoorOpener : MonoBehaviour
 {
 
     public float OpenSpeed = 10;
-    public PressurePlate PressurePlate;
+    public List<PressurePlate> PressurePlates;
+
+    private int _activationCounter = 0;
     private bool _activated = false;
 
     // Use this for initialization
     private void Start ()
     {
-        PressurePlate.AddDelegate(delegate { _activated = true; });
+        PressurePlates.ForEach(pressurePlate =>
+        {
+            pressurePlate.AddDelegate(delegate
+            {
+                _activationCounter += 1;
+                _activated = _activationCounter >= PressurePlates.Count;
+            });
+        });
     }
 
     // Update is called once per frame
