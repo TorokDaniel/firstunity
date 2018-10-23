ebd63426e4314f0c59350d138fba0a3670abacbb
Daniel Torok <torok.daniel@rarity.hu>
2018-10-23 12:21:49 +0200

    added main menu

---
+++ first unity game/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager: MonoBehaviour
+{
+    private static LevelManager _instance;
+    public static LevelManager Instance
+    {
+        get { return _instance; }
+    }
+
+    private void Start()
+    {
+        if (_instance == null)
+        {
+            _instance = this;
+            DontDestroyOnLoad(this);
+        }
+        else
+        {
+            Destroy(this);
+        }
+    }
+
+    public void LoadTutorialScene()
+    {
+        SceneManager.LoadScene(1);
+    }
+    
+}
