b50819f0b9f9f97ed5d75242621d388eae3e77fb
Daniel Torok <torok.daniel@rarity.hu>
2018-10-23 14:46:54 +0200

    added in-game menu

---
+++ first unity game/Assets/Scripts/InGameMenu.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+
+public class InGameMenu: MonoBehaviour
+{
+    private Canvas _canvas;
+
+    private void Start()
+    {
+        _canvas = GetComponent<Canvas>();
+        _canvas.enabled = false;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            _canvas.enabled = !_canvas.enabled;
+            Time.timeScale =  _canvas.enabled ? 0f : 1f;
+        }
+    }
+
+    public void OnResumeButtonClicked()
+    {
+        _canvas.enabled = false;
+        Time.timeScale = 1f;
+    }
+
+    public void OnRestartButtonClicked()
+    {
+        LevelManager.Instance.LoadTutorialScene();
+    }
+
+    public void OnExitToMenuButtonClicked()
+    {
+        LevelManager.Instance.LoadMainMenu();
+    }
+
+    public void OnExitToDesktopButtonClicked()
+    {
+        Application.Quit();
+    }
+    
+}
